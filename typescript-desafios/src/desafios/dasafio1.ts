39c442a2e821e6ee904816d24c0207dc30b3eb44
Bruna Barbieri <bruh_barbieri@hotmail.com>
2022-07-24 18:52:43 -0300

    realizando desafios

--- typescript-desafios/src/desafios/dasafio1.ts
+++ typescript-desafios/src/desafios/dasafio1.ts
@@ -1,17 +1,14 @@
-// Resposta 1
 const funcionario = {
     codigo: 10,
     nome: 'João'
 };
 
-// Resposta 2
 const funcionario2: {codigo: number, nome: string} = {
     codigo: 10,
     nome: 'joao'
 }
 
-// Respostas 3 e 4
-interface Funcionario {  // Já conhece interfaces? https://blog.logrocket.com/types-vs-interfaces-in-typescript/
+interface Funcionario {
     codigo: number,
     nome: string
 };
